06ead3a7f350b6064a63f08a39220c5537910684
dominikballreich <dominik.ballreich@codecentric.de>
2019-03-18 22:49:09 +0100

    Tidied up code

--- stacking_game_code/stacking_game_start.py
+++ stacking_game_code/stacking_game_start.py
@@ -4,7 +4,6 @@ import stack_helpers as sh
 
 
 'Start with data'
-
 # box_identificator (#Blue (0), Red (1), Yellow (2), Green (3), Orange (4)),
 # box_height,
 # box_weight,
@@ -12,7 +11,6 @@ import stack_helpers as sh
 # number_of_boxes
 boxes = np.array([[0, 50, 50, 500, 5], [1, 70, 80, 1000, 10], [2, 100, 30, 2000, 7], [3, 110, 10, 600, 9], [4, 150, 100, 600, 10]])
 non_combine = np.array([[1, 3], [4, 1], [0, 2], [3, 0]])
-
 
 # Generate items_vect
 items_vect = []
@@ -22,13 +20,13 @@ for i in range(boxes.shape[0]):
 # Create bounds for heuristic optimizer
 lb = np.zeros(np.sum(boxes[:, 4]))
 lb = np.append(lb, 0)
-ub = np.ones(np.sum(boxes[:, 4])) * 10
+ub = np.ones(np.sum(boxes[:, 4])) * 1
 ub = np.append(ub, np.sum(boxes[:, 4]))
 
 # Search for best stack
 sh.stapler.opt = True
 arguments = (boxes, items_vect, non_combine)
-vars, fopt = pso(sh.stapler, lb, ub, args=arguments, debug=True, phip=10, phig=0.99, omega=0.1, minfunc=1e-64, minstep=1e-64, maxiter=60, swarmsize=5000)
+vars, fopt = pso(sh.stapler, lb, ub, args=arguments, debug=True, phip=10, phig=0.99, omega=0.1, minfunc=1e-64, minstep=1e-64, maxiter=30, swarmsize=5000)
 
 # Return the result
 sh.stapler.opt = False
